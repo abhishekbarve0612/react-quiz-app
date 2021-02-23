e3488f185c2f64756f2ae73deb2673b5fb7286b6
abhishekbarve0612 <barveabhishek.ab@gmail.com>
2021-02-23 23:33:40 +0530

    Added Responsiveness and Categories for quiz filter(only UI)

--- src/components/quiz/QuizList.js
+++ src/components/quiz/QuizList.js
@@ -3,6 +3,14 @@ import QuizInfoCard from './QuizInfoCard';
 
 const QuizList = () => {
   return ( 
+    <div className="quiz-list-category">
+    <div className="categories">
+      <div>Abhishek</div>
+      <div>Anime</div>
+      <div>Movies</div>
+      <div>Technology</div>
+      <div>Sports</div>
+    </div>
     <div className="quiz-list">
       <QuizInfoCard 
       name="Abhishek's Quiz" 
@@ -36,24 +44,9 @@ const QuizList = () => {
       topScorer="Abhishek Barve"
       />
       
-      <QuizInfoCard 
-      name="Abhishek's Quiz" 
-      creator="Abhishek"
-      date="12-02-2022"
-      attemptCount={32}
-      topScorer="Abhishek Barve"
-      />
-      
-      <QuizInfoCard 
-      name="Abhishek's Quiz" 
-      creator="Abhishek"
-      date="12-02-2022"
-      attemptCount={32}
-      topScorer="Abhishek Barve"
-      />
-      
     </div>
-   );
+   </div>
+    );
 }
  
 export default QuizList;

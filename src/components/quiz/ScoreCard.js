5e360619f2966e528585d7f3b3f9be2543b62e65
abhishekbarve0612 <barveabhishek.ab@gmail.com>
2021-02-25 16:58:32 +0530

    Added Score %age

--- src/components/quiz/ScoreCard.js
+++ src/components/quiz/ScoreCard.js
@@ -6,12 +6,14 @@ import { Link } from "react-router-dom";
 
 const ScoreCard = () => {
   const score = useSelector(state => state.score.totalScore);
+  const qCount = useSelector(state => state.quiz.questionCount);
   const user = useSelector(state => state.quiz.user);
   return ( 
     <div className="scorecard">
       <div className="res">
         Congratulations! {user} <br/>
-        You Scored: {score}
+        You Scored: {score} <br/>
+        Your Success % is {(score / qCount * 100).toFixed(2)} %
       </div>
       <Link to="/react-quiz-app/">Go Back To Home</Link>
     </div>
